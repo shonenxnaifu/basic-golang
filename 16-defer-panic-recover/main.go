7e07a4d125a84d21ec64b4af74deb3ce103c628b
Pawitra Warda <pawitrawarda@gmail.com>
2024-09-25 14:02:30 +0700

    merge

--- 16-defer-panic-recover/main.go
+++ 16-defer-panic-recover/main.go
@@ -1,5 +1,35 @@
 package main
 
-func main()  {
-  
+import "fmt"
+
+func logging() {
+	fmt.Println("Selesai memanggil function")
+}
+
+func runApplication() {
+	defer logging()
+	fmt.Println("run application before logging")
+}
+
+func endApp() {
+	message := recover()
+	if message != nil {
+		fmt.Println("Error dengan message: ", message)
+	}
+	fmt.Println("Application ended")
+}
+
+func runApp(error bool) {
+	defer endApp()
+	if error {
+		panic("Error on Application")
+	}
+
+	fmt.Println("Application running")
+}
+
+func main() {
+	//runApplication()
+	runApp(true)
+  fmt.Println("runn")
 }
